4f2f4f6ad48df1b12328483cd96e55db91252347
Robert Carlsen <rwcarlsen@gmail.com>
2018-10-16 15:12:39 -0600

    fix bugs

--- depsolver/main.cc
+++ depsolver/main.cc
@@ -8,12 +8,13 @@
 class Node
 {
 public:
-  Node(bool elemental) : _elemental(elemental) {}
+  Node(bool elemental, const std::string & name = "") : _name(name), _elemental(elemental) {}
   void setLoop(int loop) { _loop = loop; }
   int loop() const { return _loop; }
   std::set<Node *> deps() const { return _deps; }
   std::set<Node *> dependers() const { return _dependers; }
   bool is_elemental() const { return _elemental; };
+  std::string str() { return _name; }
   Node * needs(Node * n)
   {
     _deps.insert(n);
@@ -22,6 +23,7 @@ public:
   }
 
 private:
+  std::string _name;
   bool _elemental = true;
   int _loop = std::numeric_limits<int>::max();
   std::set<Node *> _deps;
@@ -37,11 +39,8 @@ public:
   {
     std::set<Node *> rs;
     for (auto n : _nodes)
-    {
-      std::cout << "roots.size=" << rs.size() << "\n";
       if (filter(n->deps()).empty())
         rs.insert(n);
-    }
     return rs;
   }
   virtual std::set<Node *> leaves() const
@@ -52,18 +51,19 @@ public:
         leaves.insert(n);
     return leaves;
   }
-  void add(Node * n) { _nodes.insert(n); }
-  void remove(Node * n) { _nodes.erase(n); }
-  bool contains(Node * n) const { return _nodes.count(n) > 0; }
-  std::set<Node *> nodes() const { return _nodes; }
+  virtual void add(Node * n) { _nodes.insert(n); }
+  virtual void remove(Node * n) { _nodes.erase(n); }
+  virtual bool contains(Node * n) const { return _nodes.count(n) > 0; }
+  virtual std::set<Node *> nodes() const { return _nodes; }
 
 private:
   std::set<Node *> filter(std::set<Node *> ns) const
   {
+    std::set<Node *> filtered;
     for (auto n : ns)
-      if (!contains(n))
-        ns.erase(n);
-    return ns;
+      if (contains(n))
+        filtered.insert(n);
+    return filtered;
   }
   std::set<Node *> _nodes;
 };
@@ -84,19 +84,13 @@ private:
 };
 
 void
-execOrder(Subgraph & g, std::vector<std::vector<Node *>> & order)
+execOrder(Subgraph g, std::vector<std::vector<Node *>> & order)
 {
   while (!g.nodes().empty())
   {
-    std::cout << "g.nodes.size=" << g.nodes().size() << "\n";
-    std::cout.flush();
     order.push_back({});
-    std::cout << "g.roots.size=" << g.roots().size() << "\n";
-    std::cout.flush();
     for (auto n : g.roots())
     {
-      std::cout << "erasing root\n";
-      std::cout.flush();
       order.back().push_back(n);
       g.remove(n);
     }
@@ -135,9 +129,26 @@ bool
 canrun(Node * n, int candidate_loop)
 {
   for (auto dep : n->deps())
-    if (dep->loop() > candidate_loop)
+    if (!dep->is_elemental() && dep->loop() > candidate_loop)
       return false;
   return true;
+}
+
+void
+cancelUp(Node * n, Subgraph & cancelled)
+{
+  // abort recursion if any depender is not cancelled
+  if (!cancelled.contains(n))
+    for (auto dep : n->dependers())
+      if (!cancelled.contains(dep))
+        return;
+
+  // all of n's dependers have been cancelled. Cancel n and check if any of its dependencies need
+  // to be cancelled.
+  cancelled.add(n);
+  for (auto dep : n->deps())
+    if (dep->is_elemental())
+      cancelUp(dep, cancelled);
 }
 
 std::vector<std::vector<std::vector<Node *>>>
@@ -149,33 +160,29 @@ computeLoops(Graph & g)
   int loop = 0;
   while (!queue.empty())
   {
-    std::cout << "loop=" << loop << "\n";
     Subgraph g;
-    std::cout << "    spot1\n";
     for (auto n : queue)
       floodDown(n, g);
-    std::cout << "    spot2\n";
+
     for (auto n : g.leaves())
       floodUp(n, g);
-    std::cout << "    spot3\n";
 
     Subgraph cancelled;
-    for (auto n : g.roots())
+    for (auto n : g.nodes())
       if (!canrun(n, loop))
         floodDown(n, cancelled);
-    std::cout << "    spot4\n";
+
+    for (auto n : cancelled.leaves())
+      cancelUp(n, cancelled);
 
     for (auto n : cancelled.nodes())
       g.remove(n);
-    std::cout << "    spot5\n";
 
     for (auto n : g.nodes())
       n->setLoop(loop);
-    std::cout << "    spot6\n";
 
     loops.push_back({});
     execOrder(g, loops.back());
-    std::cout << "    spot7\n";
 
     // prepare for computing next loop
     queue.clear();
@@ -191,14 +198,30 @@ int
 main(int narg, char ** argv)
 {
   Graph graph;
-  auto a = graph.create(true);
-  auto b = graph.create(false);
-  auto c = graph.create(true);
+  auto a = graph.create(true, "a");
+  auto b = graph.create(false, "b");
+  auto c = graph.create(true, "c");
+  auto d = graph.create(true, "d");
   a->needs(b);
   a->needs(c);
+  a->needs(d);
   b->needs(c);
 
   auto loops = computeLoops(graph);
+
+  for (size_t i = 0; i < loops.size(); i++)
+  {
+    auto & loop = loops[i];
+    std::cout << "loop " << i + 1 << ":\n";
+    for (size_t g = 0; g < loop.size(); g++)
+    {
+      auto & group = loop[g];
+      std::cout << "    group " << i + 1 << ": ";
+      for (auto n : group)
+        std::cout << n->str() << ", ";
+      std::cout << "\n";
+    }
+  }
 
   std::cout << "a.loop = " << a->loop() << "\n";
   std::cout << "b.loop = " << b->loop() << "\n";
